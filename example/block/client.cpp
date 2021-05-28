6e50eb2c442b7daba484fd917c2f00024e883640
Tiansir-wg <1289131800@qq.com>
2021-05-28 18:41:21 +0800

    添加注释

--- example/block/client.cpp
+++ example/block/client.cpp
@@ -1,11 +1,11 @@
 // Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,16 +33,20 @@ DEFINE_string(group, "Block", "Id of the replication group");
 
 bvar::LatencyRecorder g_latency_recorder("block_client");
 
-static void* sender(void* arg) {
-    while (!brpc::IsAskedToQuit()) {
+static void *sender(void *arg)
+{
+    while (!brpc::IsAskedToQuit())
+    {
         braft::PeerId leader;
         // Select leader of the target group from RouteTable
-        if (braft::rtb::select_leader(FLAGS_group, &leader) != 0) {
+        if (braft::rtb::select_leader(FLAGS_group, &leader) != 0)
+        {
             // Leader is unknown in RouteTable. Ask RouteTable to refresh leader
             // by sending RPCs.
             butil::Status st = braft::rtb::refresh_leader(
-                        FLAGS_group, FLAGS_timeout_ms);
-            if (!st.ok()) {
+                FLAGS_group, FLAGS_timeout_ms);
+            if (!st.ok())
+            {
                 // Not sure about the leader, sleep for a while and the ask again.
                 LOG(WARNING) << "Fail to refresh_leader : " << st;
                 bthread_usleep(FLAGS_timeout_ms * 1000L);
@@ -53,7 +57,8 @@ static void* sender(void* arg) {
         // Now we known who is the leader, construct Stub and then sending
         // rpc
         brpc::Channel channel;
-        if (channel.Init(leader.addr, NULL) != 0) {
+        if (channel.Init(leader.addr, NULL) != 0)
+        {
             LOG(ERROR) << "Fail to init channel to " << leader;
             bthread_usleep(FLAGS_timeout_ms * 1000L);
             continue;
@@ -66,18 +71,25 @@ static void* sender(void* arg) {
         example::BlockRequest request;
         example::BlockResponse response;
         request.set_offset(butil::fast_rand_less_than(
-                            FLAGS_block_size - FLAGS_request_size));
-        const char* op = NULL;
-        if (butil::fast_rand_less_than(100) < (size_t)FLAGS_write_percentage) {
+            FLAGS_block_size - FLAGS_request_size));
+        const char *op = NULL;
+        // 按照设置的读写请求比例发送读或写请求
+        if (butil::fast_rand_less_than(100) < (size_t)FLAGS_write_percentage)
+        {
             op = "write";
             cntl.request_attachment().resize(FLAGS_request_size, 'a');
+            // 发送写请求
             stub.write(&cntl, &request, &response, NULL);
-        } else {
+        }
+        else
+        {
             op = "read";
             request.set_size(FLAGS_request_size);
+            // 发送读请求
             stub.read(&cntl, &request, &response, NULL);
         }
-        if (cntl.Failed()) {
+        if (cntl.Failed())
+        {
             LOG(WARNING) << "Fail to send request to " << leader
                          << " : " << cntl.ErrorText();
             // Clear leadership since this RPC failed.
@@ -85,17 +97,20 @@ static void* sender(void* arg) {
             bthread_usleep(FLAGS_timeout_ms * 1000L);
             continue;
         }
-        if (!response.success()) {
+        if (!response.success())
+        {
             LOG(WARNING) << "Fail to send request to " << leader
                          << ", redirecting to "
-                         << (response.has_redirect() 
-                                ? response.redirect() : "nowhere");
+                         << (response.has_redirect()
+                                 ? response.redirect()
+                                 : "nowhere");
             // Update route table since we have redirect information
             braft::rtb::update_leader(FLAGS_group, response.redirect());
             continue;
         }
         g_latency_recorder << cntl.latency_us();
-        if (FLAGS_log_each_request) {
+        if (FLAGS_log_each_request)
+        {
             LOG(INFO) << "Received response from " << leader
                       << " op=" << op
                       << " offset=" << request.offset()
@@ -110,12 +125,14 @@ static void* sender(void* arg) {
     return NULL;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
     butil::AtExitManager exit_manager;
 
     // Register configuration of target group to RouteTable
-    if (braft::rtb::update_configuration(FLAGS_group, FLAGS_conf) != 0) {
+    if (braft::rtb::update_configuration(FLAGS_group, FLAGS_conf) != 0)
+    {
         LOG(ERROR) << "Fail to register configuration " << FLAGS_conf
                    << " of group " << FLAGS_group;
         return -1;
@@ -123,36 +140,48 @@ int main(int argc, char* argv[]) {
 
     std::vector<bthread_t> tids;
     tids.resize(FLAGS_thread_num);
-    if (!FLAGS_use_bthread) {
-        for (int i = 0; i < FLAGS_thread_num; ++i) {
-            if (pthread_create(&tids[i], NULL, sender, NULL) != 0) {
+    if (!FLAGS_use_bthread)
+    {
+        for (int i = 0; i < FLAGS_thread_num; ++i)
+        {
+            if (pthread_create(&tids[i], NULL, sender, NULL) != 0)
+            {
                 LOG(ERROR) << "Fail to create pthread";
                 return -1;
             }
         }
-    } else {
-        for (int i = 0; i < FLAGS_thread_num; ++i) {
-            if (bthread_start_background(&tids[i], NULL, sender, NULL) != 0) {
+    }
+    else
+    {
+        for (int i = 0; i < FLAGS_thread_num; ++i)
+        {
+            if (bthread_start_background(&tids[i], NULL, sender, NULL) != 0)
+            {
                 LOG(ERROR) << "Fail to create bthread";
                 return -1;
             }
         }
     }
 
-    while (!brpc::IsAskedToQuit()) {
+    while (!brpc::IsAskedToQuit())
+    {
         sleep(1);
         LOG_IF(INFO, !FLAGS_log_each_request)
-                << "Sending Request to " << FLAGS_group
-                << " (" << FLAGS_conf << ')'
-                << " at qps=" << g_latency_recorder.qps(1)
-                << " latency=" << g_latency_recorder.latency(1);
+            << "Sending Request to " << FLAGS_group
+            << " (" << FLAGS_conf << ')'
+            << " at qps=" << g_latency_recorder.qps(1)
+            << " latency=" << g_latency_recorder.latency(1);
     }
 
     LOG(INFO) << "Block client is going to quit";
-    for (int i = 0; i < FLAGS_thread_num; ++i) {
-        if (!FLAGS_use_bthread) {
+    for (int i = 0; i < FLAGS_thread_num; ++i)
+    {
+        if (!FLAGS_use_bthread)
+        {
             pthread_join(tids[i], NULL);
-        } else {
+        }
+        else
+        {
             bthread_join(tids[i], NULL);
         }
     }
--- example/block/run_server.sh
+++ example/block/run_server.sh
@@ -66,3 +66,4 @@ for ((i=0; i<$FLAGS_server_num; ++i)); do
         -port=$((${FLAGS_port}+i)) -conf="${raft_peers}" > std.log 2>&1 &
     cd ../..
 done
+# gdbserver :2333 ./block_server -bthread_concurrency=18 -crash_on_fatal_log=true -raft_max_segment_size=8388608 -raft_sync=true -port=8200 -conf=192.168.108.2:8200:0192.168.108.2:8201:0192.168.108.2:8202:0
